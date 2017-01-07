33caec5c8a1c9a08574d103dd84004f43e856217
Raul Herranz Muñoz <Raul Herranz Muoz>
2017-01-07 16:51:46 +0100

    Adding ex0101

---
+++ ex0101/ex0101/ex0101/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ex0101.Views;
+using Xamarin.Forms;
+
+namespace ex0101
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new MainView();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ex0101/ex0101/ex0101/Views/MainView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ex0101.Views
+{
+    public partial class MainView : ContentPage
+    {
+        int count = 0;
+
+        public MainView()
+        {
+            InitializeComponent();
+
+            MyButton.Clicked += (sender, args) =>
+            {
+                MyButton.Text = string.Format("{0} clicks", count++);
+            };
+        }
+    }
+}
